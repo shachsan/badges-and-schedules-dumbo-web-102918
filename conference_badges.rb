a9c1e5a69e4e1a95f74ee4cd050f19178a6f8a60
SANJAY K SAI <shachsan@hotmail.com>
2018-10-16 01:25:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,6 +7,8 @@ def batch_badge_creator(attendees)
   attendees.map {|attendee|"Hello, my name is #{attendee}."}
 end
 
+batch_badge_creator(["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"])
+
 def assign_rooms(attendees)
   attendees.map.with_index {|attendee, i|"Hello, #{attendee}! You'll be assigned to room #{i+1}!"
 end
