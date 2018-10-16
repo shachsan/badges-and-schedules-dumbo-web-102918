5064c23620ba55d1a1f74da4beacb96819647232
SANJAY K SAI <shachsan@hotmail.com>
2018-10-16 01:38:30 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,6 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees)
+  puts batch_badge_creator(attendees).join("\n")
   puts assign_rooms(attendees)
 end
