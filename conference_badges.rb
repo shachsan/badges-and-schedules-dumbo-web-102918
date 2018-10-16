ab0a4d7cfb0fdd24fcdb885b64317c510be80b86
SANJAY K SAI <shachsan@hotmail.com>
2018-10-16 02:05:17 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  puts batch_badge_creator(attendees).join("\n")
+  new_line = batch_badge_creator(attendees)#.join("\n")
+  puts new_line.join("\n")
   puts assign_rooms(attendees)
   # attendees.each {|attendee|puts "Hello, my name is #{attendee}." }
   # attendees.each_with_index {|attendee, i|puts "Hello, #{attendee}! You'll be assigned to room #{i+1}!"}
